b7f255306bae449f0bcb8cb5ab3063fc9da25226
antondubek <makepeaceanthony@gmail.com>
2018-10-13 20:04:10 +0100

    Sound introduced and animation working

---
+++ src/Sound.java
@@ -0,0 +1,50 @@
+import com.phidget22.VoltageInputVoltageChangeEvent;
+import com.phidget22.VoltageInputVoltageChangeListener;
+import processing.core.PApplet;
+
+public class Sound extends PApplet implements VoltageInputVoltageChangeListener {
+
+    private PApplet p;
+    private Game game;
+
+    private int maxStacks = 20;
+    private int counter = 1;
+    private int display = 0;
+
+    private int r = 0;
+    private int g = 191;
+    private int b = 255;
+
+    public Sound(PApplet p, Game game) {
+        this.p = p;
+        this.game = game;
+    }
+
+    public void draw(){
+        p.noStroke();
+
+        if(counter <= maxStacks) {
+            for (float i = 1; i < counter; i++) {
+
+                p.fill(p.lerpColor(p.color(240,255,0), p.color(255,0,0), ((i)/counter)));
+                p.rect(((game.getWidth())/5) * 4, ((game.getHeight()/2)+140) - (i * 15), 140, 10);
+
+            }
+        } else {
+            for (int i = 1; i < maxStacks; i++) {
+                p.fill(0, 255, 0);
+                p.rect(((game.getWidth())/5) * 4, ((game.getHeight()/2)+140) - (i * 15), 140, 10);
+
+                //Debug this to only produce 1 incrmenent not 300
+                this.display++;
+            }
+        }
+    }
+
+    @Override
+    public void onVoltageChange(VoltageInputVoltageChangeEvent event) {
+        double input = event.getVoltage() * 1000;
+        counter =  (int) (1 + (input - 60)*(20-1)/(400-60));
+
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -13,17 +13,24 @@ public class Test extends PApplet{
     VoltageRatioInput chLin2;
     DigitalOutput redLED;
     DigitalInput click;
+    VoltageInput sound;
+
     RCServo servo;
+
 
     Game game = new Game(1800, 600, 51,51,51);
 
     Ministick myBall = new Ministick(this, game,0,0);
     Rotation myArc = new Rotation(this, game);
     Click myClick = new Click(this, game);
+    Sound mySound = new Sound(this, game);
 
 
     public void settings(){
         size(game.getWidth(),game.getHeight());
+
+        int phidgetNo = 274077;
+        int servoNo = 306007;
 
         // Makes things look nicer with some Anti-Aliasing
         smooth();
@@ -35,17 +42,20 @@ public class Test extends PApplet{
             redLED = new DigitalOutput();
             servo = new RCServo();
             click = new DigitalInput();
+            sound = new VoltageInput();
 
-            chRota.setDeviceSerialNumber(274077);
-            chLin1.setDeviceSerialNumber(274077);
-            chLin2.setDeviceSerialNumber(274077);
-            redLED.setDeviceSerialNumber(274077);
-            servo.setDeviceSerialNumber(306007);
-            click.setDeviceSerialNumber(274077);
+            chRota.setDeviceSerialNumber(phidgetNo);
+            chLin1.setDeviceSerialNumber(phidgetNo);
+            chLin2.setDeviceSerialNumber(phidgetNo);
+            redLED.setDeviceSerialNumber(phidgetNo);
+            sound.setDeviceSerialNumber(phidgetNo);
+            servo.setDeviceSerialNumber(servoNo);
+            click.setDeviceSerialNumber(phidgetNo);
 
             chRota.setChannel(0);
             chLin1.setChannel(1);
             chLin2.setChannel(2);
+            sound.setChannel(3);
             redLED.setChannel(0);
             servo.setChannel(0);
             click.setChannel(0);
@@ -56,6 +66,7 @@ public class Test extends PApplet{
             chLin1.open();
             chLin2.open();
             redLED.open();
+            sound.open();
             servo.open(5000);
             click.open();
 
@@ -63,6 +74,7 @@ public class Test extends PApplet{
             chLin1.addVoltageRatioChangeListener(myBall);
             chLin2.addVoltageRatioChangeListener(myBall);
             click.addStateChangeListener(myClick);
+            sound.addVoltageChangeListener(mySound);
 
             redLED.setState(true);
             servo.setTargetPosition(0);
@@ -86,6 +98,7 @@ public class Test extends PApplet{
         myBall.draw();
         myArc.draw();
         myClick.draw();
+        mySound.draw();
 
 
     }
